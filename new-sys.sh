a6e94cc1668d112d521f86f9fc6454096184616d
gitluin <kyluin@outlook.com>
2021-03-30 14:59:33 -0400

    code: realized i need multihead support too

--- new-sys.sh
+++ new-sys.sh
@@ -1,52 +1,46 @@
 #!/bin/bash
 
 # Module text is saved in this format:
-#	%{l}\n
-#	{ModuleName}abcdef{ModuleName-}\n
-#	%{c}\n
-#	{ModuleName}ghijkl{ModuleName-}\n
-#	%{r}\n
-#	{ModuleName}mnopqr{ModuleName-}\n
+#	%{S0}%{l}{{ModuleName}}abcdef{{ModuleName-}}%{r}{{ModuleName}}ghijkl{{ModuleName-}}\n
+#	%{S1}%{l}{{ModuleName}}mnopqr{{ModuleName-}}%{r}{{ModuleName}}stuvwx{{ModuleName-}}
 #
 # End goal is to output this:
-#	printf "%s\n" "%{l}$MODDELIMabcdef$MODDELIM%{c}$MODDELIMghijkl$MODDELIM%{r}$MODDELIMmnopqr$MODDELIM"
+#	"%{S0}%{l}$MODDELIMabcdef$MODDELIM%{r}$MODDELIMghijkl$MODDELIM%{S1}%{l}$MODDELIMmnopqr$MODDELIM%{r}$MODDELIMstuvwx$MODDELIM"
 
 # TODO: Make it clear that you can't have certain strings in your bar text, or else sed will fuck with it
+# TODO: Make it clear that this most likely requires GNU sed
 
+# Only operates on single monlines
 UpdateModuleText() {
-	# TODO: Check this preserves newlines, etc.
 	LOCAL_MODULE_CONTENTS="$1"
 	MODULE="$2"
-	NEWTEXT="$($MODULE)"
+	NEWTEXT="$($MODULE "$3")"
 
-	LOCAL_MODULE_CONTENTS="$(echo -e $LOCAL_MODULE_CONTENTS | \
-		sed "s/\({Module$MODULE}\).*\({Module$MODULE-}\)/\1$NEWTEXT\2/")"
-
-	# restore formatting
-	LOCAL_MODULE_CONTENTS="$(echo $LOCAL_MODULE_CONTENTS | sed 's/} /}\\n/g' | sed 's/$/\\n/')"
+	LOCAL_MODULE_CONTENTS="$(echo $LOCAL_MODULE_CONTENTS | \
+		sed "s/\({{Module$MODULE}}\).*\({{Module$MODULE-}}\)/\1$NEWTEXT\2/")"
 
 	echo "$LOCAL_MODULE_CONTENTS"
 }
 
+
+# TODO: how does monitor-dependent output get treated? Say, for ParseSara?
+# 	TODO: Preferably without special treatment, i.e. as if it were any other module
+# 	Do all modules get told what monitor they are on?
+
+
 # TODO: Front and back MODDELIM
+#	Just separate sed calls with [^-}}]}} and [^}}]}}
 PrintStatus() {
 	LOCAL_MODULE_CONTENTS="$1"
 	LOCAL_MODDELIM="$2"
 
-	# Replace module tags, enclose module text
-	STATUSLINE="$(echo -e $LOCAL_MODULE_CONTENTS | sed 's/{Module.*}\(.*\){Module.*-}/{{\1}}/g')"
-
-	# Replace induced spaces with $LOCAL_MODDELIM
-	STATUSLINE="$(echo $STATUSLINE | sed "s/} {/}$LOCAL_MODDELIM{/g" | \
-		sed "s/} %/}$LOCAL_MODDELIM%/g" | \
-		sed "s/$/$LOCAL_MODDELIM/")"
-
-	# Remove module text enclosers
-	STATUSLINE="$(echo $STATUSLINE | sed 's/{{//g' | sed 's/}}//g')"
+	# Replace {{ModuleName}} and {{ModuleName-}} tags with $MODDELIMS
+	STATUSLINE="$(echo "$LOCAL_MODULE_CONTENTS" | sed "s/{{[^}}]*}}/$LOCAL_MODDELIM/g")"
 
 	printf "%s\n" "$STATUSLINE"
 }
 
+# TODO: update for new multihead format
 InitStatus() {
 	LOCAL_MODULES="$1"
 	LOCAL_MODULE_CONTENTS=""
@@ -56,9 +50,9 @@ InitStatus() {
 	for ALIGN in $ALIGNMENTS; do
 		ALIGNMODS="$(echo -e $MODSLIST | grep "%{$ALIGN}" | sed 's/%{.}//')"
 
-		ALIGN_OUT="%{$ALIGN}\n"
+		ALIGN_OUT="%{$ALIGN}"
 		for ALIGNMOD in $ALIGNMODS; do
-			ALIGN_OUT="$ALIGN_OUT{Module$ALIGNMOD}$($ALIGNMOD){Module$ALIGNMOD-}\n"
+			ALIGN_OUT="$ALIGN_OUT{{Module$ALIGNMOD}}$($ALIGNMOD){{Module$ALIGNMOD-}}"
 		done
 
 		LOCAL_MODULE_CONTENTS="$LOCAL_MODULE_CONTENTS$ALIGN_OUT"
@@ -78,8 +72,9 @@ InitStatus() {
 #	c. peachbar-sys.sh reads from a fifo that tells it what needs updating.
 #		basically, a queue of to-dos.
 #		peachbar-sys.sh < $PEACHFIFO | lemonbar | sh
-#		exec "sara-interceptor.sh $SARAFIFO"
-#		i. each module has a sleep that forks off and then writes the module name to $PEACHFIFO when done.
+#		exec "sara-interceptor.sh $SARAFIFO $PEACHFIFO"
+#		i. each module has a sleep that forks off and then writes the module name to
+#			$PEACHFIFO when done.
 #			peachbar-sys.sh while read line; do's things.
 #			if receive "All", then updates entire bar
 #			if nothing in $PEACHFIFO, no work is done!
@@ -104,9 +99,26 @@ PrintStatus "$MODULE_CONTENTS" "$MODDELIM"
 # Main Loop
 # ------------------------------------------
 while read line; do
+	MULTI="$(xrandr -q | grep " connected" | wc -l)"
+
 	# $line is a module name
-	test "$line" != "All" && MODULE_CONTENTS="$(UpdateModuleText "$MODULE_CONTENTS" "$line")" || \
+	if test "$line" != "All"; then
+		for (( i=0; i<$MULTI; i++ )); do
+			# TODO: test
+			MON_MODULE_CONTENTS="$(echo -e $MODULE_CONTENTS | grep "%{S$i}")"
+			MON_MODULE_CONTENTS="$(UpdateModuleText "$MON_MODULE_CONTENTS" "$line" $i)"
+
+			# overwrite old monline with new monline
+			MODULE_CONTENTS="$(echo -e $MODULE_CONTENTS | \
+				sed "s/%{S$i}.*/%{S$i}$MON_MODULE_CONTENTS/")"
+
+			# TODO: restore formatting
+
+			TO_OUT="${TO_OUT}%{B$BARBG}%{S$i}${STATUSLINE}%{B-}"
+		done
+	else
 		MODULE_CONTENTS="$(InitStatus)"
+	fi
 
 	PrintStatus "$MODULE_CONTENTS" "$MODDELIM"
 done
