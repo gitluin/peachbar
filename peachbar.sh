6f75cbbcdccfb611f17d9c30739bfaa4a6be5643
gitluin <kyluin@outlook.com>
2021-04-19 23:18:04 -0400

    code: revert, test later

--- peachbar.sh
+++ peachbar.sh
@@ -34,4 +34,12 @@ NUMMON="$(xrandr -q | grep ' connected' | wc -l)"
 NUMFIELDS="$(($NUMTAGS + $(($NUMMOD * $NUMCLICKPERMOD)) + 1))"
 NUMCLICK="$(($NUMFIELDS * $NUMMON))"
 
-peachbar-sys.sh < "$PEACHFIFO" | lemonbar -a $NUMCLICK -g "$BARW"x"$BARH"+"$BARX"+"$BARY" -d -f "$BARFONT" -f "$ICONFONT" -B "$BARBG" -F "$BARFG" | sh &
+peachbar-sys.sh < "$PEACHFIFO" | lemonbar \
+	-a $NUMCLICK \
+	-g "$BARW"x"$BARH"+"$BARX"+"$BARY" \
+	-d \
+	-f "$BARFONT" \
+	-f "$ICONFONT" \
+	-B "$BARBG" \
+	-F "$BARFG" \
+	| sh &
